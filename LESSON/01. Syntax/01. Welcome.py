15eba251057a850fed6b5c481884072f655864f2
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-10-17 15:59:33 +0300

    Ioannis E. Kommas

--- LESSON/01. Syntax/01. Welcome.py
+++ LESSON/01. Syntax/01. Welcome.py
@@ -1,4 +1,4 @@
 #  Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved.
 # Αλλάξτε το PyCademy με το όνομά σας. Εκτελέστε τον κώδικα για να δείτε τι κάνει!
-my_name = "PyCademy"
+my_name = "My Name"
 print("Hello and welcome " + "\n" + my_name + "!")
