44458253119a8be2a0d58b6e9d547e79fd3aca20
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-10-18 20:48:22 +0300

    Ioannis E. Kommas

--- PROJECT/01. Syntax/solution.py
+++ PROJECT/01. Syntax/solution.py
@@ -1,3 +1,53 @@
 #  Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved.
 
+"1.	Let’s add in our first item, the Lovely Seat that is the store’s namesake. Create a variable called ls_description"
+"and assign to it the following string:  Lovely Seat "
 
+"2.	Great, now let’s create a price for the lovely Seat. Create a variable ls_price and set it equal to: 254.00"
+
+"3.	Let’s extend our inventory with another characteristic piece of furniture! Create a variable called ss_description"
+"and assign to it the following string: Stylish Settee"
+
+"4.	Now let’s set the price for our Stylish Settee. Create a variable ss_price and assign it the value of: 180.50"
+
+"5.	Fantastic, we just need one more item before we’re ready for business. Create a new variable called ll_description "
+"and assign it the following: Luxurious Lamp "
+
+"6.	Let’s set the price for this item. Create a variable called ll_price and set it equal to: 52.15"
+
+"7.	In order to be a business, we should also be calculating sales tax. Let’s store that in a variable as well. Define "
+"the variable sales_tax and set it equal to .24 . That’s 24%.Our First Customer "
+
+"8.	Our first customer is making their purchase! Let’s keep a running tally of their expenses by defining a variable "
+"called customer_one_total . Since they haven’t purchased anything yet, let’s set that variable equal to 0 for now. "
+
+"9.	We should also keep a list of the descriptions of things they’re purchasing. Create a variable called "
+"customer_one_items and set that equal to the empty string. We will tack on the descriptions to this as they make "
+"their purchases. "
+
+"10. Our customer has decided they are going to purchase our Lovely Seat! Add the price to customer_one_total"
+
+"11. Let’s start keeping track of the items our customer purchased. Add the description of the Lovely Seat to "
+"customer_one_items "
+
+"12. Our customer has also decided to purchase the Luxurious Lamp! Let’s add the price to the customer_one_total."
+
+"13. Let’s keep the itemization up-to-date and add the description of the Luxurious Lamp to our customer_one_items"
+
+"14. They’re ready to check out! Let’s begin by calculating sales tax. Create a variable called customer_one_tax and "
+"set it equal to customer_one_total times sales_tax. "
+
+"15. Add the sales_tax to customer_one_total."
+
+"16. Let’s start printing up their receipt! Begin by printing out the heading for their itemization. Print the phrase "
+"'Customer One Items' "
+
+"17. Print customer_one_items"
+
+"18. Now add a heading for their total cost: Print out 'Customer One Total'"
+
+"19. Now print out their total! Our first customer now has a receipt for the things they purchased."
+
+"20. Congratulations! We created our catalog and served our first customer. We used our knowledge of strings and " \
+"numbers to create and update variables. We were able to print out an itemized list and a total cost for our " \
+"customer. Lovely! "
