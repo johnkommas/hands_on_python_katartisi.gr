7e5c38686e7f6ca8e1bd4131492824d9fae4f19e
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-10-19 13:33:06 +0300

    Ioannis E. Kommas

---
+++ LESSON/02. Functions/04. Whitespace.py
@@ -0,0 +1,6 @@
+def about_this_computer():
+    print("This computer is running on version Everest Puma")
+    print("This is your desktop")
+
+
+about_this_computer()
---
+++ LESSON/02. Functions/05. Parameters.py
@@ -0,0 +1,6 @@
+def mult_two_add_three():
+    number = 5
+    print(number)
+
+
+mult_two_add_three()
