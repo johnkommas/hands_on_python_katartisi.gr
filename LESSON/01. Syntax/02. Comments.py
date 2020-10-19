b0e2ce9a8979aa3a731e925aa596119ad8e805e5
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-10-19 21:55:55 +0300

    Ioannis E. Kommas

--- LESSON/01. Syntax/02. Comments.py
+++ LESSON/01. Syntax/02. Comments.py
@@ -1,5 +1,7 @@
 #  Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved.
-# Αυτή η μεταβλητή θα χρησιμοποιηθεί για να μετρήσει τον αριθμό των φορών που κάποιος κάνει tweet στη λέξη persnickety
+# Αυτή η μεταβλητή θα χρησιμοποιηθεί για να μετρήσει
+# τον αριθμό των φορών που κάποιος κάνει tweet
+# στη λέξη persnickety
 persnickety_count = 0
 
 # Αυτός ο κωδικός θα υπολογίσει την πιθανότητα αύριο να βρέξει
@@ -9,3 +11,14 @@ complicated_rain_calculation_for_tomorrow = 'Yes'
 useful_value = complicated_rain_calculation_for_tomorrow
 
 # Γράψτε εδώ ένα σχόλιο που περιγράφει το πρώτο πρόγραμμα που θέλετε να γράψετε
+
+def greet_customer(grocery_store, special_item):
+    print(f"""
+    Welcome
+    to
+    {grocery_store}
+    Our
+    special is {special_item}.Have
+    fun
+    shopping!""")
+greet_customer("Stu's Staples", "papayas")
---
+++ LESSON/02. Functions/06. Multiple Parameters.py
@@ -0,0 +1,2 @@
+def mult_two_add_three(number):
+    print(2 * number + 3)
