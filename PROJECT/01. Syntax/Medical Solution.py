a4704c2b2c173cd97c454239c3d72f2ca09c53d0
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-10-28 10:57:01 +0200

    Ioannis E. Kommas

---
+++ PROJECT/01. Syntax/Medical Solution.py
@@ -0,0 +1,23 @@
+# TODO 1. Create Variables
+
+# TODO 2. initialize formula and Calculate
+
+# TODO 3. Display
+
+# TODO 4. Play with AGE
+
+# TODO 5. Recalculate Formula
+
+# TODO 6. Find the Difference
+
+# TODO 7. Display
+
+# TODO 8. Reset Values , Play with BMI
+
+# TODO 9. Recalculate Formula / Find the Difference / Display
+
+# TODO 10. Reset Values , Play with SEX
+
+# TODO 11. Recalculate Formula / Find the Difference / Display
+
+# TODO 13. Extra Practise
