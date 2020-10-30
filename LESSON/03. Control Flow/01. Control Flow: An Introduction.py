0afa9e0029fc8a887d40e34e0434cbc23352831f
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-10-30 12:01:45 +0200

    Ioannis E. Kommas

---
+++ LESSON/03. Control Flow/01. Control Flow: An Introduction.py
@@ -0,0 +1,15 @@
+
+"""
+Προσδιορίστε εάν οι ακόλουθες δηλώσεις είναι δυαδικές εκφράσεις ή όχι. Εάν είναι, ορίστε τη μεταβλητή αντιστοίχισης
+προς τα δεξιά στο "Ναι" και αν δεν ορίσετε τη μεταβλητή σε "Όχι". Ακολουθεί ένα παράδειγμα του τι πρέπει να κάνετε:
+"""
+"My dog is the cutest dog in the world."
+example_statement = "Όχι"
+"Dogs are mammals."
+statement_one =""
+"My dog is named Pavel."
+statement_two =""
+"Dogs make the best pets."
+statement_three =""
+"Cats are female dogs."
+statement_four =""
