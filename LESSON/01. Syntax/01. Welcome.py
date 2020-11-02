040d758b7b92dcc16552a91a0eb8b6dcf89171e1
infokommas <kommas@infosector.gr>
2020-11-02 17:39:42 +0200

    elounda

--- LESSON/01. Syntax/01. Welcome.py
+++ LESSON/01. Syntax/01. Welcome.py
@@ -1,5 +1,4 @@
 #  Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved.
-
 # ------------------- ΑΣΚΗΣΗ ---------------------
 # TODO 1. Αλλάξτε το "My Name" με το όνομά σας. Εκτελέστε τον κώδικα για να δείτε τι κάνει!
 my_name = "My Name"
